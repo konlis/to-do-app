fd7cada9be18c960911a2beabbf607f5719fb222
Konrad Lis <o.konradlis@gmail.com>
2019-11-23 18:00:02 +0100

    Add state modification

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,8 +5,12 @@ import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
+import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
+    state = {
+    columns: this.props.columns || [],
+  }
     static propTypes = {
     title: PropTypes.node.isRequired,
     description: PropTypes.node,
@@ -27,11 +31,12 @@ static defaultProps = {
           {ReactHtmlParser(this.props.description)}
           </div>
           <div className={styles.columns}>
-          {this.props.columns.map(item => (
-                  <Column key={item.title} title={item.title}/>
-
-))}
-
+          {this.state.columns.map(({key, ...columnProps}) => (
+              <Column key={key} {...columnProps} />
+          ))}
+          </div>
+          <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
           </div>
         </section>
     )
