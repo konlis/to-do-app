6cd1ea6119479402a0640f67d1ed41110eba7126
Konrad Lis <o.konradlis@gmail.com>
2019-12-14 10:24:58 +0100

    Solve prioblerm with heroku (Bad import at FaqContainer)

--- src/components/SearchResult/SearchResultContainer.js
+++ src/components/SearchResult/SearchResultContainer.js
@@ -2,10 +2,17 @@ import { connect } from 'react-redux';
 import { getCardsForSearch, createAction_changeSearchString } from '../../redux/cardsRedux.js';
 import SearchResult from './SearchResult.js';
 
-const mapStateToProps = (state, props, searchString) => ({
-  cards: getCardsForSearch(state, props.match.params.searchString ),
-  filteredCards: state.cards.filter(card => card.title.includes(searchString)),
-});
+const mapStateToProps = (state, props, searchString) => {
+  const cards = getCardsForSearch(state, props.match.params.searchString);
+  console.log('cards', cards);
+  const filteredCards = state.cards.filter(card => card.title.includes(searchString));
+  console.log('filteredcards', filteredCards);
+  
+  return {
+    cards: cards,
+    filteredCards: filteredCards,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => ({
   changeSearchString: newSearchString => dispatch(createAction_changeSearchString(newSearchString)),
